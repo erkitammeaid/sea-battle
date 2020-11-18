d231e63fb57677c2753859d65e81e24adc6835c1
erkitammeaid <erkipoiss01@gmail.com>
2020-11-18 08:47:42 +0200

    ssss

--- app.js
+++ app.js
@@ -98,22 +98,21 @@ function initShip ( length, player ) {
     }
 }
 
-for ( let len = 4; len > 0; len-- ) {
-    for ( let i = 1; i <= 5-len; i++ ) {
-        initShip(len, 'human')
-        initShip(len, 'machine')
-   }
-}
-
-console.log(gameBoard)
-
 function initGrid( b ) {
+    const boardName = b.dataset.boardName
     for ( let i = 0; i < 10; i++ ) {
         for ( let j = 0; j < 10; j++ ) {
             let cellDiv = document.createElement('div')
             cellDiv.classList = 'cell'
             cellDiv.dataset.row = i
-            cellDiv.dataset.row = j
+            cellDiv.dataset.col = j
+
+            if ( boardName == 'human' ) {
+                if ( gameBoard[boardName][i][j].charAt(0) == 's' ) {
+                    cellDiv.classList += ' ship'
+                    // cellDiv.innerHTML = gameBoard[boardName][i][j]
+                }
+            }
 
             b.append(cellDiv)
         }
@@ -122,14 +121,29 @@ function initGrid( b ) {
 
 const humanBoardDiv = document.querySelector('#human-board')
 const machineBoardDiv = document.querySelector('#machine-board')
+
+for ( let len = 4; len > 0; len-- ) {
+    for ( let i = 1; i <= 5-len; i++ ) {
+        initShip(len, 'human')
+        initShip(len, 'machine')
+   }
+}
+
 initGrid(humanBoardDiv)
 initGrid(machineBoardDiv)
 
-for ( let len = 4; len > 0; len--) {
-    for ( let i = 1; i <= 5-len; i++) {
-        initShip(len, 'human')
-        initShip(len, 'machine')
-    }
-}
+const cellDivs = document.querySelectorAll('#machine-board .cell')
+
+cellDivs.forEach(el => {
+    el.addEventListener('click', event => {
+        const selectedCell = event.currentTarget
+        const selectedRow = selectedCell.dataset.row
+        const selectedCol = selectedCell.dataset.col
+
+        console.log(gameBoard['machine'][selectedRow][selectedCol])
+    })
+})
+
+
 
 console.log(gameBoard)
